1d3cfdf09a683dfc58f911f193c5631dd9a70e5c
IanoSang <ianosang18@gmail.com>
2022-04-23 19:21:16 +0300

    feat: intances and classmethods- adds intances to save and a method decorators for deleting users

--- passwordlock.py
+++ passwordlock.py
@@ -10,3 +10,19 @@ class User:
         """
         self.username = username
         self.password = password
+
+    def save_user(self):
+        """
+        A method that saves a new user instance into the user list
+        """
+        User.user_list.append(self)
+
+    @classmethod
+    def display_user(cls):
+        return cls.user_list
+
+    def delete_user(self):
+        """
+        delete_account method deletes a  saved account from the list
+        """
+        User.user_list.remove(self)
