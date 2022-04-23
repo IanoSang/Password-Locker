db8935ccbfa9866eabf13759b43e11953370056b
IanoSang <ianosang18@gmail.com>
2022-04-24 00:14:50 +0300

    Feat: Test- adds a method that runs before each test and test if the intances are initialized correctly

--- passwordlock_test.py
+++ passwordlock_test.py
@@ -34,6 +34,20 @@ class TestCredentials(unittest.TestCase):
     A test class that defines test cases for credentials class
     """
 
+    def setUp(self):
+        """
+        Method that runs before each individual credentials test methods run.
+        """
+        self.new_credential = Credentials('Twitter', 'IanSang', 'Is24446666668888888')
+
+    def test_init(self):
+        """
+        Test case to check if a new Credentials instance has been initialized correctly
+        """
+        self.assertEqual(self.new_credential.account, 'Twitter')
+        self.assertEqual(self.new_credential.username, 'IanSang')
+        self.assertEqual(self.new_credential.password, 'Is24446666668888888')
+
 
 if __name__ == "__main__":
     unittest.main()
