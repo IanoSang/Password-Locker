0396f68c1c7bf8de2e32dd2431c5b9dd575443d5
IanoSang <ianosang18@gmail.com>
2022-04-24 01:07:01 +0300

    feat- test_delete - test method to test if we can remove an account credentials from our credentials_list

--- passwordlock_test.py
+++ passwordlock_test.py
@@ -70,6 +70,17 @@ class TestCredentials(unittest.TestCase):
         test_credential.save_details()
         self.assertEqual(len(Credentials.credentials_list), 2)
 
+    def test_delete_credential(self):
+        """
+        test method to test if we can remove an account credentials from our credentials_list
+        """
+        self.new_credential.save_details()
+        test_credential = Credentials("Gmail", "ianosang", "Tebengonik25")
+        test_credential.save_details()
+
+        self.new_credential.delete_credentials()
+        self.assertEqual(len(Credentials.credentials_list), 1)
+
 
 if __name__ == "__main__":
     unittest.main()
