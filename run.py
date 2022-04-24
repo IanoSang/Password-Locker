95016388280c18df7a2352f4207aec4c213a357a
IanoSang <ianosang18@gmail.com>
2022-04-24 15:46:17 +0300

    feat: create_new_user- adds a Function to create a new user

--- run.py
+++ run.py
@@ -3,4 +3,9 @@
 from passwordlock import User, Credentials
 
 
-
+def create_new_user(username, password):
+    """
+    Function to create a new user
+    """
+    new_user = User(username, password)
+    return new_user
