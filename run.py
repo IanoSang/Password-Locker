2667705889b8ba2e20a542566daf1b249b5d80b6
IanoSang <ianosang18@gmail.com>
2022-04-24 16:08:38 +0300

    feat: display_account_details- adds a Function that returns all the saved credential

--- run.py
+++ run.py
@@ -47,3 +47,10 @@ def save_credentials(credentials):
     Function to save Credentials to the credentials list
     """
     credentials.save_details()
+
+
+def display_accounts_details():
+    """
+    Function that returns all the saved credential.
+    """
+    return Credentials.display_credentials()
